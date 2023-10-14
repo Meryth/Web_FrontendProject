5b9b2aac9fddbb7965424b7dad2880540b09a8ca
Elna Halilovic <elna.halilovic@diamir.io>
2023-10-14 14:06:13 +0200

    setup simple form, multi-pages wip

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 // vite.config.js
-export default {
-  build: {
-    minify: true,
-  }
-  // config options
-}
+import {resolve} from 'path'
+import {defineConfig} from "vite";
+
+export default defineConfig({
+    build: {
+        minify: true,
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                login: resolve(__dirname, 'src/html/login.html')
+            }
+        }
+    }
+    // config options
+})
