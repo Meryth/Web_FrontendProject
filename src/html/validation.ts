91e6fe42eadf310bf608c508c4247924da277904
Elna Halilovic <elna.halilovic@diamir.io>
2023-10-17 20:04:13 +0200

    improve validation

--- src/html/validation.ts
+++ src/html/validation.ts
@@ -1,11 +1,14 @@
 export function checkUsername() {
-    let username = (<HTMLInputElement>document.getElementById("username")).value;
-
-    if (username?.includes("/")) {
-        let errorMessage = 'Username must not contain "/"!';
-        const usernameError = document.getElementById("username_error");
-        usernameError!.textContent = errorMessage;
-    }
+    let username = (<HTMLInputElement>document.getElementById("username"));
+    let usernameError = document.getElementById("username_error");
+    username.addEventListener('input', event => {
+        event.preventDefault()
+        if (username.value.includes('/')) {
+            usernameError!.textContent = 'Username must not contain "/"!';
+        } else {
+            usernameError!.textContent = '';
+        }
+    })
 }
 
 document.getElementById("submit_btn")?.addEventListener("click", () => checkUsername())
