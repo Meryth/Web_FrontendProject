dfa03634ee64db0a4a763f4baf5c438972879bfa
Elna Halilovic <elna.halilovic@diamir.io>
2023-10-24 21:36:46 +0200

    rename api.ts to home.ts, create table, add favorite function

---
+++ src/js/home.ts
@@ -0,0 +1,82 @@
+//API doc: https://api.chucknorris.io/
+export class JokeManager {
+    joke: string
+
+    constructor() {
+        this.joke = ""
+    }
+
+    async getRandomJoke() {
+        try {
+            let response = await fetch("https://api.chucknorris.io/jokes/random")
+            const joke = document.getElementById("joke")
+
+            if (!response.ok) {
+                throw new Error("Error: " + response.status)
+            }
+
+            let data = await response.json()
+            this.joke = data.value
+            joke!.textContent = this.joke
+
+            return this
+        } catch (e) {
+            console.log(e)
+        }
+    }
+
+    favoriteJoke() {
+        const favTable = (<HTMLTableElement>document.getElementById("fav_table"))
+
+        let row = favTable.insertRow(-1)
+
+        let c1 = row.insertCell(0)
+        let c2 = row.insertCell(1)
+
+        c1.textContent = "Star"
+        c2.textContent = this.joke
+        return this
+    }
+
+    addJokeToFavs() {
+        const btnFav = document.getElementById("btn_fav")
+
+        if (btnFav) {
+            btnFav.addEventListener('click', () => {
+                let table = (<HTMLTableElement>document.getElementById("fav_table"))
+                let isJokeFavorited = this.checkJokeAlreadyFavorited(table)
+
+                if (!isJokeFavorited) {
+                    this.favoriteJoke()
+                }
+            })
+        }
+
+        return this
+    }
+
+    getNewJoke() {
+        const generateJokeButton = document.getElementById("generate_joke_btn")
+        generateJokeButton!.addEventListener('click', event => {
+            event.preventDefault()
+            void this.getRandomJoke()
+        })
+
+        return this
+    }
+
+    checkJokeAlreadyFavorited(table: HTMLTableElement) {
+        for (let i = 0; i < table.rows.length; i++) {
+            for (let j = 0; j < table.rows[i].cells.length; j++) {
+                if (table.rows[i].cells[j].textContent === this.joke) {
+                    return true
+                }
+            }
+        }
+        return false;
+    }
+}
+
+const jokeManager = new JokeManager()
+jokeManager.addJokeToFavs()
+jokeManager.getNewJoke()
