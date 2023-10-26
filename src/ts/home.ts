087bef3c588d3191dffd3b6e9a3299aec7cdcb95
Elna Halilovic <elna.halilovic@diamir.io>
2023-10-26 15:28:44 +0200

    add and use linting and prettier tools

--- src/ts/home.ts
+++ src/ts/home.ts
@@ -1,95 +1,95 @@
 //API doc: https://api.chucknorris.io/
 export class JokeManager {
-    joke: string
+  joke: string;
 
-    constructor() {
-        this.joke = ""
+  constructor() {
+    this.joke = '';
+  }
+
+  async getRandomJoke() {
+    try {
+      const response = await fetch('https://api.chucknorris.io/jokes/random');
+      const joke = document.getElementById('joke');
+
+      if (!response.ok) {
+        throw new Error('Error: ' + response.status);
+      }
+
+      const data = await response.json();
+      this.joke = data.value;
+      joke!.textContent = this.joke;
+
+      return this;
+    } catch (e) {
+      console.log(e);
     }
+  }
 
-    async getRandomJoke() {
-        try {
-            let response = await fetch("https://api.chucknorris.io/jokes/random")
-            const joke = document.getElementById("joke")
+  favoriteJoke() {
+    const favTable = (<HTMLTableElement>document.getElementById('fav_table'));
 
-            if (!response.ok) {
-                throw new Error("Error: " + response.status)
-            }
+    const row = favTable.insertRow(-1);
 
-            let data = await response.json()
-            this.joke = data.value
-            joke!.textContent = this.joke
+    const c1 = row.insertCell(0);
+    const c2 = row.insertCell(1);
 
-            return this
-        } catch (e) {
-            console.log(e)
+    const removeJokeButton = document.createElement('button');
+    removeJokeButton.id = 'btn_remove_joke';
+    removeJokeButton.textContent = 'REMOVE';
+
+    removeJokeButton.addEventListener('click', () => {
+      const selectedRow = removeJokeButton.closest('tr');
+      if (selectedRow) {
+        selectedRow.remove();
+      }
+    });
+
+    c1.appendChild(removeJokeButton);
+    c2.textContent = this.joke;
+    return this;
+  }
+
+  addJokeToFavs() {
+    const btnFav = document.getElementById('btn_fav');
+
+    if (btnFav) {
+      btnFav.addEventListener('click', () => {
+        const table = (<HTMLTableElement>document.getElementById('fav_table'));
+        const isJokeFavorited = this.checkJokeAlreadyFavorited(table);
+
+        if (!isJokeFavorited) {
+          this.favoriteJoke();
         }
+      });
     }
 
-    favoriteJoke() {
-        const favTable = (<HTMLTableElement>document.getElementById("fav_table"))
+    return this;
+  }
 
-        let row = favTable.insertRow(-1)
+  getNewJoke() {
+    const generateJokeButton = document.getElementById('generate_joke_btn');
+    generateJokeButton!.addEventListener('click', async event => {
+      event.preventDefault();
+      await this.getRandomJoke();
+    });
 
-        let c1 = row.insertCell(0)
-        let c2 = row.insertCell(1)
+    return this;
+  }
 
-        let removeJokeButton = document.createElement("button")
-        removeJokeButton.id = "btn_remove_joke"
-        removeJokeButton.textContent = "REMOVE"
-
-        removeJokeButton.addEventListener('click', () => {
-            let selectedRow = removeJokeButton.closest('tr')
-            if (selectedRow) {
-                selectedRow.remove()
-            }
-        })
-
-        c1.appendChild(removeJokeButton)
-        c2.textContent = this.joke
-        return this
-    }
-
-    addJokeToFavs() {
-        const btnFav = document.getElementById("btn_fav")
-
-        if (btnFav) {
-            btnFav.addEventListener('click', () => {
-                let table = (<HTMLTableElement>document.getElementById("fav_table"))
-                let isJokeFavorited = this.checkJokeAlreadyFavorited(table)
-
-                if (!isJokeFavorited) {
-                    this.favoriteJoke()
-                }
-            })
+  checkJokeAlreadyFavorited(table: HTMLTableElement) {
+    for (let i = 0; i < table.rows.length; i++) {
+      for (let j = 0; j < table.rows[i].cells.length; j++) {
+        if (table.rows[i].cells[j].textContent === this.joke) {
+          return true;
         }
-
-        return this
+      }
     }
-
-    getNewJoke() {
-        const generateJokeButton = document.getElementById("generate_joke_btn")
-        generateJokeButton!.addEventListener('click', async event => {
-            event.preventDefault()
-            await this.getRandomJoke()
-        })
-
-        return this
-    }
-
-    checkJokeAlreadyFavorited(table: HTMLTableElement) {
-        for (let i = 0; i < table.rows.length; i++) {
-            for (let j = 0; j < table.rows[i].cells.length; j++) {
-                if (table.rows[i].cells[j].textContent === this.joke) {
-                    return true
-                }
-            }
-        }
-        return false;
-    }
+    return false;
+  }
 }
 
-const jokeManager = new JokeManager()
+const jokeManager = new JokeManager();
 
-void jokeManager.getRandomJoke()
-jokeManager.addJokeToFavs()
-jokeManager.getNewJoke()
+void jokeManager.getRandomJoke();
+jokeManager.addJokeToFavs();
+jokeManager.getNewJoke();
--- src/ts/validation.ts
+++ src/ts/validation.ts
@@ -1,55 +1,55 @@
 //TODO: refactor to avoid duplicate code
 export function checkEmail() {
-    let email = (<HTMLInputElement>document.getElementById("email"))
-    let emailError = document.getElementById("email_error")
+  const email = (<HTMLInputElement>document.getElementById('email'));
+  const emailError = document.getElementById('email_error');
 
-    const emailRegex = new RegExp("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$")
+  const emailRegex = new RegExp('^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$');
 
-    email.addEventListener('focusout', event => {
-        event.preventDefault()
-        if (emailRegex.test(email.value)) {
-            emailError!.textContent = ""
-            toggleSubmitDisabled(false)
-        } else {
-            emailError!.textContent = "Invalid Email!"
-            toggleSubmitDisabled(true)
-        }
-    })
+  email.addEventListener('focusout', event => {
+    event.preventDefault();
+    if (emailRegex.test(email.value)) {
+      emailError!.textContent = '';
+      toggleSubmitDisabled(false);
+    } else {
+      emailError!.textContent = 'Invalid Email!';
+      toggleSubmitDisabled(true);
+    }
+  });
 }
 
 export function checkPhoneNumber() {
-    let phoneNumber = (<HTMLInputElement>document.getElementById("phone_nr"))
-    let phoneNumberError = document.getElementById("phone_nr_error")
+  const phoneNumber = (<HTMLInputElement>document.getElementById('phone_nr'));
+  const phoneNumberError = document.getElementById('phone_nr_error');
 
-    const phoneNumberRegex = new RegExp("^[\\+]?[(]?[0-9]{3}[)]?[-\\s\\.]?[0-9]{3}[-\\s\\.]?[0-9]{4,6}$")
+  const phoneNumberRegex = new RegExp('^[\\+]?[(]?[0-9]{3}[)]?[-\\s\\.]?[0-9]{3}[-\\s\\.]?[0-9]{4,6}$');
 
-    phoneNumber.addEventListener('focusout', event => {
-        event.preventDefault()
-        if (phoneNumberRegex.test(phoneNumber.value)) {
-            phoneNumberError!.textContent = ""
-            toggleSubmitDisabled(false)
-        } else {
-            phoneNumberError!.textContent = "Invalid Phone Number!"
-            toggleSubmitDisabled(true)
-        }
-    })
+  phoneNumber.addEventListener('focusout', event => {
+    event.preventDefault();
+    if (phoneNumberRegex.test(phoneNumber.value)) {
+      phoneNumberError!.textContent = '';
+      toggleSubmitDisabled(false);
+    } else {
+      phoneNumberError!.textContent = 'Invalid Phone Number!';
+      toggleSubmitDisabled(true);
+    }
+  });
 }
 
 export function toggleSubmitDisabled(disabled: boolean) {
-    let submitButton = (<HTMLInputElement>document.getElementById("submit_btn"))
-    submitButton.disabled = disabled
+  const submitButton = (<HTMLInputElement>document.getElementById('submit_btn'));
+  submitButton.disabled = disabled;
 }
 
 export function login() {
-    let submitButton = (<HTMLInputElement>document.getElementById("submit_btn"))
-    if (submitButton) {
-        submitButton.addEventListener('click', event => {
-            event.preventDefault()
-            location.href = "../html/home.html"
-        })
-    }
+  const submitButton = (<HTMLInputElement>document.getElementById('submit_btn'));
+  if (submitButton) {
+    submitButton.addEventListener('click', event => {
+      event.preventDefault();
+      location.href = '../html/home.html';
+    });
+  }
 }
 
-checkEmail()
-checkPhoneNumber()
-login()
+checkEmail();
+checkPhoneNumber();
+login();
