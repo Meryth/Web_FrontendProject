9d805eb226f6e36fec9ca5dde008330c70816749
Elna Halilovic <elna.halilovic@diamir.io>
2023-10-27 11:27:33 +0200

    improve input validation

--- src/ts/validation.ts
+++ src/ts/validation.ts
@@ -1,39 +1,73 @@
-//TODO: refactor to avoid duplicate code
-export function checkEmail() {
-  const email = (<HTMLInputElement>document.getElementById('email'));
-  const emailError = document.getElementById('email_error');
+export class ValidationManager {
+  isInputValid: boolean;
 
-  const emailRegex = new RegExp('^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$');
+  constructor() {
+    this.isInputValid = false;
+  }
 
-  email.addEventListener('focusout', event => {
-    event.preventDefault();
-    if (emailRegex.test(email.value)) {
-      emailError!.textContent = '';
-      toggleSubmitDisabled(false);
-    } else {
-      emailError!.textContent = 'Invalid Email!';
-      toggleSubmitDisabled(true);
+  checkInput(
+    inputElement: HTMLInputElement,
+    errorElement: HTMLElement,
+    regex: RegExp,
+    errorText: string,
+  ) {
+    inputElement.addEventListener('input', event => {
+      event.preventDefault();
+      if (regex.test(inputElement.value)) {
+        errorElement!.textContent = '';
+        this.isInputValid = true;
+      } else {
+        errorElement!.textContent = errorText;
+        this.isInputValid = false;
+      }
+    });
+    return this;
+  }
+
+  checkEmail() {
+    const email = (<HTMLInputElement>document.getElementById('email'));
+    const emailError = (<HTMLElement>document.getElementById('email_error'));
+    const emailRegex = new RegExp('^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$');
+    const errorText = 'Invalid Email!';
+
+    this.checkInput(email, emailError, emailRegex, errorText);
+    return this;
+  }
+
+  checkPhoneNumber() {
+    const phoneNumber = (<HTMLInputElement>document.getElementById('phone_nr'));
+    const phoneNumberError = (<HTMLElement>document.getElementById('phone_nr_error'));
+    const phoneNumberRegex = new RegExp('^[\\+]?[(]?[0-9]{3}[)]?[-\\s\\.]?[0-9]{3}[-\\s\\.]?[0-9]{4,6}$');
+    const errorText = 'Invalid Phone number!';
+
+    this.checkInput(phoneNumber, phoneNumberError, phoneNumberRegex, errorText);
+    return this;
+  }
+
+  checkAllRequiredFieldsFilled() {
+    const form = document.getElementById('login_form')!;
+    const requiredFields = form.querySelectorAll('[required]') as NodeListOf<HTMLInputElement>;
+    for (const field of requiredFields) {
+      if (!field.value.trim()) {
+        return false;
+      }
     }
-  });
+    return true;
+  }
+
+  setSubmitButtonState() {
+    const form = document.getElementById('login_form')!;
+    form.addEventListener('input', () => {
+      if (this.isInputValid && this.checkAllRequiredFieldsFilled()) {
+        toggleSubmitDisabled(false);
+      } else {
+        toggleSubmitDisabled(true);
+      }
+    });
+    return;
+  }
 }
 
-export function checkPhoneNumber() {
-  const phoneNumber = (<HTMLInputElement>document.getElementById('phone_nr'));
-  const phoneNumberError = document.getElementById('phone_nr_error');
-
-  const phoneNumberRegex = new RegExp('^[\\+]?[(]?[0-9]{3}[)]?[-\\s\\.]?[0-9]{3}[-\\s\\.]?[0-9]{4,6}$');
-
-  phoneNumber.addEventListener('focusout', event => {
-    event.preventDefault();
-    if (phoneNumberRegex.test(phoneNumber.value)) {
-      phoneNumberError!.textContent = '';
-      toggleSubmitDisabled(false);
-    } else {
-      phoneNumberError!.textContent = 'Invalid Phone Number!';
-      toggleSubmitDisabled(true);
-    }
-  });
-}
 
 export function toggleSubmitDisabled(disabled: boolean) {
   const submitButton = (<HTMLInputElement>document.getElementById('submit_btn'));
@@ -50,6 +84,9 @@ export function login() {
   }
 }
 
-checkEmail();
-checkPhoneNumber();
+const validationManager = new ValidationManager();
+
+validationManager.checkEmail();
+validationManager.checkPhoneNumber();
+validationManager.setSubmitButtonState();
 login();
